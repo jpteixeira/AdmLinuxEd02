f50d9f6568783f0cef5a1b33480cba6a4ed35e53
Amador Pahim <amador@pahim.org>
2011-07-15 11:20:32 -0300

    First commit

---
+++ ExercicioShell01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Este script é um exercício para o Curso Administração de SO Linux
+# da Universidade de Caxias do Sul.
+#
+
+# Passo 1
+# Informe quantos ARQUIVOS existem no diretório "/etc", somente no 1º nível.
+# Solicite <enter> para prosseguir.
+
+
+# Passo 2
+# Informe qual o modelo e frequência do processador da sua máquina. Essa informação está em "/proc/cpuinfo".
+# Solicite <enter> para prosseguir.
+
+
+# Passo 3
+# Informe a situação do roteamento, se está ou não habilitado. Essa informação está em "/proc/sys/net/ipv4/ip_forward". 
+# Solicite <enter> para prosseguir.
+
+
+# Passo 4
+# 
+# Solicite <enter> para encerrar o script.
